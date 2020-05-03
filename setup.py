07235f12104828f42588f352fd93a4383ddacbf0
Gustav Rasmussen <gcr84@hotmail.com>
2020-05-03 20:26:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+
+
+
+version=version,
+description=('Create tables and modify records'),
+long_description=long_description,
+url='https://github.com/Gustav-Rasmussen/sqlite-app',
+author='<AUTHOR>'
+author_email='<EMAIL>'
+license='MIT',
+include_package_data=True,
+classifiers=[
+    'Development Status :: 4 - Beta',
+],
+py_modules = ['sqlite_app',]
